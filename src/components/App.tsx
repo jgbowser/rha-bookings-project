c313d6da7b43ff6e287a9a14412624864b4bfa7e
John Bowser <john.bow7720@gmail.com>
2023-06-05 22:23:03 -0600

    add skeleton components for the basic app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
-import '../App.css'
+import './App.css'
 
 import { FaCalendarAlt, FaDoorOpen, FaUsers } from 'react-icons/fa'
 
