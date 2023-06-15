8dc2d447086543b50a16cd8d733e588a4a38488f
John Bowser <john.bow7720@gmail.com>
2023-06-14 21:24:09 -0400

    add next button to the bookables list component

--- src/components/Bookables/BookablesList.tsx
+++ src/components/Bookables/BookablesList.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { FaArrowRight } from 'react-icons/fa'
 import { bookables } from '../../static.json'
 
 export default function BookablesList() {
@@ -6,15 +7,25 @@ export default function BookablesList() {
   const group = 'Rooms'
   const bookablesInGroup = bookables.filter(b => b.group === group)
 
+  function nextBookable() {
+    setBookableIndex(i => ((i + 1) % bookablesInGroup.length))
+  }
+
   return (
-    <ul className="bookables items-list-nav">
-      {bookablesInGroup.map((b, i) => (
-        <li key={b.id} className={i === bookableIndex ? 'selected' : ''}>
-          <button className="btn" onClick={() => setBookableIndex(i)}>
-            {b.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul className="bookables items-list-nav">
+        {bookablesInGroup.map((b, i) => (
+          <li key={b.id} className={i === bookableIndex ? 'selected' : ''}>
+            <button className="btn" onClick={() => setBookableIndex(i)}>
+              {b.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button className="btn" onClick={nextBookable} autoFocus>
+        <FaArrowRight />
+        <span>Next</span>
+      </button>
+    </div>
   )
 }
